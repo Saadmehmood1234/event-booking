c86366f138e18045d833a47db810887386d66124
Saad Mehmood <158064428+Saadmehmood1234@users.noreply.github.com>
2025-05-15 10:40:41 +0530

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -26,7 +26,7 @@ const Nav = async () => {
       <Link href="/">
         <div className="flex items-center gap-2">
           <Rocket className="w-8 h-8 text-amber-400" />
-          <span className="text-xl font-bold text-gray-200">Sonu Don</span>
+          <span className="text-xl font-bold text-gray-200">EventEase</span>
         </div>
       </Link>
 
